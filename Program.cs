de08983f3f6ca82d8c5c1050150ca72cc268ee70
Fandiño <111803876+DFL100@users.noreply.github.com>
2023-12-08 19:56:14 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -199,6 +199,173 @@ namespace _02Boletin
 
             }
 
+            if (ejercicioxd == 14)
+            {
+
+                Funciones.printEnunciado(14);
+
+                int[] a14 = new int[100];
+                double c14 = 0;
+                Random generador = new Random();
+
+                for (int i = 1; i < 100; i++)
+                {
+
+                    a14[i] = generador.Next(1, 9999);
+                    // double b14 = a14[i] * a14[i];
+                    double b14 = Math.Sqrt(a14[i]);
+                    Console.WriteLine("El cuadrado de la casilla "+ i + " es " + b14);
+
+                    c14 += b14;
+
+                    
+                }
+                Console.WriteLine("La suma de los cuadrados es " + c14);
+            }
+
+            if (ejercicioxd == 15)
+            {
+
+                Funciones.printEnunciado(15);
+
+                int[] a15 = new int[100];
+                int b15 = 0;
+                Random generador = new Random();
+
+                for (int i = 1; i < 100; i++)
+                {
+
+                    a15[i] = generador.Next();
+                    
+                    Console.WriteLine("El valor de la casilla " + i + " es " + a15[i]);
+                    
+                    if (a15[i] >= 0)
+                    {
+
+                        b15 += a15[i];
+
+                    }
+                }
+                Console.WriteLine("La suma de los números positivos es " + b15);
+
+            }
+
+            if (ejercicioxd == 17)
+            {
+
+                Funciones.printEnunciado(17);
+
+                int[] a17 = new int[100];
+                int b17 = 0;
+                int c17 = 0;
+                Random generador = new Random();
+
+                for (int i = 1; i < 100; i++)
+                {
+
+                    a17[i] = generador.Next();
+
+                    Console.WriteLine("El valor de la casilla " + i + " es " + a17[i]);
+
+                    if (a17[i] % 2 != 0 )
+                    {
+
+                        b17++;
+
+                    }
+                }
+                c17 = (b17 * 100) / 100;
+                Console.WriteLine("El porcentaje de números impares es " + c17 + "%");
+
+            }
+
+            if (ejercicioxd == 26)
+            {
+
+                double[] a26 = new double[100];
+                double A26 = 0;
+                double[] b26 = new double[100];
+                double B26 = 0;
+                double c26 = 0;
+
+                Random generador = new Random();
+
+                a26[0] = 5;
+                for (int i = 1;i < a26.Length; i++)
+                {
+
+                    a26[i] = generador.Next();
+
+                    Console.WriteLine("El valor " + i + " de a26 es " + a26[i]);
+                    A26 += a26[i];
+
+                }
+                Console.WriteLine("La suma de todas las celdas de a26 es " + A26);
+
+                Console.ReadLine();
+
+                b26[0] = 5;
+
+                for (int i = 1; i < b26.Length; i++)
+                {
+
+                    b26[i] = generador.Next();
+
+                    Console.WriteLine("El valor " + i + " de b26 es " + b26[i]);
+                    B26 += b26[i];
+
+                }
+                Console.WriteLine("La suma de todas las celdas de b26 es " + B26);
+
+                for (int i = 0; i <100;  i++)
+                {
+
+                    if (a26[i] == b26[i])
+                    {
+
+                        Console.WriteLine("La cuadrícula " + i + " es igual en las dos arrays");
+
+                    }
+                    else
+                    {
+
+                        Console.WriteLine("La cuadrícula " + i + " no es igual en las dos arrays");
+                        c26++;
+
+                    }
+
+                }
+
+                Console.ReadLine();
+               
+                if (A26 == B26)
+                {
+
+                    Console.WriteLine("La suma de las dos arrays es la misma");
+
+                }
+                else
+                {
+
+                    Console.WriteLine("La suma de las dos arrays no es la misma");
+
+                }
+
+                Console.ReadLine();
+                if (c26 != 0)
+                {
+
+                    Console.WriteLine("Las arrays son distintas");
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Las arrays son iguales");
+
+                }
+                
+            }
 
             Console.ReadLine();
         }
